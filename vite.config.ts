9d429c148b4d2ea6cb8df82c8b28604002a2acfe
Enzo Pinotti <enzopinottii@gmail.com>
2026-05-17 23:39:10 -0300

    fix: remover configuracion de preprocesador css en vite config para compilacion infalible en Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
-      } as any,
-    },
-  },
 })
